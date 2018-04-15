de23fcb8f35ec46266735ee46932dbc8473dc3d3
butlerjonathan <36801326+butlerjonathan@users.noreply.github.com>
2018-04-14 20:26:19 -0400

    rough start

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+var quiz =  [{
+    qustion: "What is the name of the main Character",
+    choices: ["Kine", "Tommy", "Jonny", "Ghost", "None"],
+    correctanswer: "Ghost"
+},{
+
+    qustion: "What popular rapper stars and produce this show",
+    choices: ["50 cent", "Master P", "Rick Ross", "Jay-z", "Nick Cannon"],
+    correctanswer: "50 cent"
+},{
+
+    qustion: "",
+    choices: ["50 cent", "Master P", "Rick Ross", "Jay-z", "Nick Cannon"],
+    correctanswer: "50 cent"
+},{
+
+    qustion: "What popular rapper stars and produce this show",
+    choices: ["50 cent", "Master P", "Rick Ross", "Jay-z", "Nick Cannon"],
+    correctanswer: "50 cent"
+},{
+
+
+    qustion: "What popular rapper stars and produce this show",
+    choices: ["50 cent", "Master P", "Rick Ross", "Jay-z", "Nick Cannon"],
+    correctanswer: "50 cent"
+},{
+
+
+    qustion: "What popular rapper stars and produce this show",
+    choices: ["50 cent", "Master P", "Rick Ross", "Jay-z", "Nick Cannon"],
+    correctanswer: "50 cent"
+},{
+
+
+    
+
+}];
