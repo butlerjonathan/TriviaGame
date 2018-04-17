02a81c7313ec9b6af7fd3e840689331d4a710981
butlerjonathan <36801326+butlerjonathan@users.noreply.github.com>
2018-04-16 20:58:02 -0400

    final push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,153 @@
-var quiz =  [{
-    qustion: "What is the name of the main Character",
-    choices: ["Kine", "Tommy", "Jonny", "Ghost", "None"],
-    correctanswer: "Ghost"
+var id=triviaQuestions = [{
+	question: "What is the main Charecter nick name?",
+	answerList: ["Kane", "Ghost", "James", "Shorty"],
+	answer: 1
 },{
-
-    qustion: "What popular rapper stars and produce this show",
-    choices: ["50 cent", "Master P", "Rick Ross", "Jay-z", "Nick Cannon"],
-    correctanswer: "50 cent"
+	question: "What is Tommy Favorite car?",
+	answerList: ["Mustange", "Camaro", "Charger", "F-250"],
+	answer: 0
 },{
-
-    qustion: "",
-    choices: ["50 cent", "Master P", "Rick Ross", "Jay-z", "Nick Cannon"],
-    correctanswer: "50 cent"
+	question: "What is Ghost main line of work?",
+	answerList: ["Club Owner", "Drugs", "FBI Agent", "Hit Man"],
+	answer: 0
 },{
-
-    qustion: "What popular rapper stars and produce this show",
-    choices: ["50 cent", "Master P", "Rick Ross", "Jay-z", "Nick Cannon"],
-    correctanswer: "50 cent"
+	question: "Who is Tommy to Ghost?",
+	answerList: ["Brother", "Cousin", "Friend", "Nephew"],
+	answer: 2
 },{
-
-
-    qustion: "What popular rapper stars and produce this show",
-    choices: ["50 cent", "Master P", "Rick Ross", "Jay-z", "Nick Cannon"],
-    correctanswer: "50 cent"
+	question: "Why was Ghost on death row",
+	answerList: ["Drug", "Set-up", "Murder", "None"],
+	answer: 3
 },{
-
-
-    qustion: "What popular rapper stars and produce this show",
-    choices: ["50 cent", "Master P", "Rick Ross", "Jay-z", "Nick Cannon"],
-    correctanswer: "50 cent"
+	question: "Which rapper help produce the show?",
+	answerList: ["50 cent", "lil wayne", "Flow-rider", "T.I"],
+	answer: 0
 },{
-
-
-    
-
+	question: "Who Kane take under his wings",
+	answerList: ["The daughter", "The son", "His son", "Tommy"],
+	answer: 1
+},{
+	question: "What was the name of the club in the movie?",
+	answerList: ["Heat", "Rock Bottom", "Truth", "None"],
+	answer: 2
+},{
 }];
+
+var search = [];
+var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
+var messages = {
+	correct: "Yes, that's right!",
+	incorrect: "No, that's not it.",
+	endTime: "Out of time!",
+	finished: "Alright! Let's see how well you did."
+}
+
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#unanswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+	newQuestion();
+}
+
+function newQuestion(){
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+	answered = true;
+	
+	//questions & answerList
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(triviaQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
+	}
+	countdown();
+	//answer will pause the time and setup answerPage
+	$('.thisChoice').on('click',function(){
+		userSelect = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
+
+function countdown(){
+	seconds = 15;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown(){
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 5000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 5000);
+	}	
+}
+
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
